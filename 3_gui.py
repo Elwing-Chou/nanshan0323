85be2adc3f1c9ce2e20dd549449f35abe38537d3
Elwing-Chou <birdfan8814@gmail.com>
2022-03-23 10:57:57 +0800

    Add files via upload

---
+++ 3_gui.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+# e = 元件(父親元件)
+# e.排版()  pack(上下左右) grid(表格) absolute(絕對)
+
+window = tk.Tk()
+window.geometry("500x500+100+100")
+# 先創Frame
+f1 = tk.Frame(window)
+f1.pack()
+# 標籤: Label
+l1 = tk.Label(f1, text="輸入身高:")
+l1.pack()
+# 單行輸入: Entry
+e1 = tk.Entry(f1)
+e1.pack()
+# 標籤: Label
+l2 = tk.Label(f1, text="輸入體重:")
+l2.pack()
+# 單行輸入: Entry
+e2 = tk.Entry(f1)
+e2.pack()
+# 按紐: Button
+b1 = tk.Button(f1, text="計算")
+b1.pack()
+result = tk.Label(f1, text="按上面按鈕計算...")
+result.pack()
+window.mainloop()
