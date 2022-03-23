39a95d6f5bb89aacc0db0fbd48230079e4b5c7f4
Elwing-Chou <birdfan8814@gmail.com>
2022-03-23 11:05:58 +0800

    Add files via upload

--- 3_gui.py
+++ 3_gui.py
@@ -7,22 +7,23 @@ window = tk.Tk()
 window.geometry("500x500+100+100")
 # 先創Frame
 f1 = tk.Frame(window)
-f1.pack()
+f1.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
 # 標籤: Label
 l1 = tk.Label(f1, text="輸入身高:")
-l1.pack()
+l1.pack(expand=True, fill=tk.BOTH, pady=10)
 # 單行輸入: Entry
 e1 = tk.Entry(f1)
-e1.pack()
+e1.pack(expand=True, fill=tk.BOTH, pady=10)
 # 標籤: Label
 l2 = tk.Label(f1, text="輸入體重:")
-l2.pack()
+l2.pack(expand=True, fill=tk.BOTH, pady=10)
 # 單行輸入: Entry
 e2 = tk.Entry(f1)
-e2.pack()
+e2.pack(expand=True, fill=tk.BOTH, pady=10)
 # 按紐: Button
 b1 = tk.Button(f1, text="計算")
-b1.pack()
+# tk.X
+b1.pack(expand=True, fill=tk.BOTH, pady=10)
 result = tk.Label(f1, text="按上面按鈕計算...")
-result.pack()
+result.pack(expand=True, fill=tk.BOTH, pady=10)
 window.mainloop()
